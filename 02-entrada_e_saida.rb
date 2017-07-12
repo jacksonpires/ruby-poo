e5b8d70f44a0cbba089d98d2f0eabbccd9b876ae
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2017-07-12 19:15:11 -0300

    02 - Entrada e Saída

---
+++ 02-entrada_e_saida.rb
@@ -0,0 +1,9 @@
+
+puts "Olá!"
+puts "Digite o seu nome:"
+v1 = gets.chomp # Essa linha serve para ler a entrada do teclado.
+puts "O seu nome é " + v1
+
+puts "Digite sua idade:"
+v2 = gets.chomp
+puts "Sua idade é: " + v2
