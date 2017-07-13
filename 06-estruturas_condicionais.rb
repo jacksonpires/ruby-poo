12f1165ab508defb7403fd0c7027b6a1363c6870
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2017-07-13 18:59:37 -0300

    06 - Estruturas condicionais

---
+++ 06-estruturas_condicionais.rb
@@ -0,0 +1,38 @@
+puts "Escolha um número entre 1 e 5"
+v1 = gets.chomp.to_i
+
+# Condicional SE / IF
+=begin
+if v1 > 10 then
+  puts "O valor digitado é maior que 10"
+elsif v1 >= 5
+  puts "O valor é maior que 5"
+else
+  puts "O valor digitado é menor ou igual 5"
+end
+=end
+
+# A menos que
+=begin
+unless v1 > 10
+  puts "O número digitado é menor que 10"
+else
+  puts "O número digitado é maior que 10"
+end
+=end
+
+case v1
+  when 1
+    puts "Vc escolheu a opção 1"
+  when 2
+    puts "Vc escolheu a opção 2"
+  when 3
+    puts "Vc escolheu a opção 3"
+  when 4
+    puts "Vc escolheu a opção 4"
+  when 5
+    puts "Vc escolheu a opção 5"
+  else
+    puts "Opção inválida!!"
+end
+
