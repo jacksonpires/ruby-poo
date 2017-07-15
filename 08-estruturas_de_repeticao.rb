5bfe977abc936719dc5f29008e538df611b403f5
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2017-07-15 15:03:09 -0300

    08 - Estruturas de Repetição

---
+++ 08-estruturas_de_repeticao.rb
@@ -0,0 +1,15 @@
+i = 1
+
+while i <= 50 # enquanto for verdadeiro
+  puts "jackson - #{i}"
+  i += 1
+end
+
+# i == 50
+
+until i <= 0 # enquanto for falso
+  puts "sou falso - #{i}"
+  i -= 1
+end
+
+puts "fim!"
