6b519f980267163daec6e31593b10f578fd680a0
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2017-08-11 19:42:35 -0300

    Argument Vector

--- trabalhando_com_arquivos/app.rb
+++ trabalhando_com_arquivos/app.rb
@@ -1,5 +1,10 @@
- File.open('teste.txt', 'r') do |arq|
+
+if ARGV.size > 0
+ File.open(ARGV[0], 'r') do |arq|
    while line = arq.gets
      puts line
    end
  end
+else
+  puts "Você deve informar o nome do arquivo a ser aberto. Ex: ruby app.rb teste.txt"
+end
