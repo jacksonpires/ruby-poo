b6ef25bc46d3b2c33227e186a337fab95ba20633
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2017-08-11 22:20:04 -0300

    Gems para terminal

---
+++ jogos/adivinhar_numero/Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'tty-cursor'
+gem 'tty-spinner'
--- jogos/adivinhar_numero/lib/inicializacao.rb
+++ jogos/adivinhar_numero/lib/inicializacao.rb
@@ -1,13 +1,25 @@
+require 'tty-cursor'
+require 'tty-spinner'
+
 class Inicializacao
   def self.inicializando
     system('clear')
 
-    print "Inicializando."
-    4.times do |i|
-      sleep 1
-      print "."
-    end
-    puts "."
+    cursor = TTY::Cursor
+    print cursor.move_to(60, 15)
+
+    spinner = TTY::Spinner.new
+    spinner = TTY::Spinner.new("[:spinner] Carregando ...", format: :pulse_2)
+    spinner.auto_spin # Automatic animation with default interval
+    sleep(5) # Perform task
+    spinner.stop('Pronto!') # Stop animation
+
+    # print "Inicializando."
+    # 4.times do |i|
+    #   sleep 1
+    #   print "."
+    # end
+    # puts "."
 
     # File.open(File.expand_path('../../ascii/bat.txt', __FILE__), 'r') do |arq|
     #   while line = arq.gets
