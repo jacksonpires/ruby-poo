1b97b7942eca0199e8e604a2d0f15fd42382c36d
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2017-08-11 22:05:22 -0300

    ASCII Art

--- jogos/adivinhar_numero/lib/inicializacao.rb
+++ jogos/adivinhar_numero/lib/inicializacao.rb
@@ -1,10 +1,24 @@
 class Inicializacao
   def self.inicializando
+    system('clear')
+
     print "Inicializando."
     4.times do |i|
       sleep 1
       print "."
     end
     puts "."
+
+    # File.open(File.expand_path('../../ascii/bat.txt', __FILE__), 'r') do |arq|
+    #   while line = arq.gets
+    #    puts line
+    #   end
+    # end
+
+    # sleep 3
+
+    system('clear')
   end
 end
+
+
